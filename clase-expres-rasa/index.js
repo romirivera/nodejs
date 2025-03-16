9ccdc5b5b391b7c8ddbe2c73dbb45ccc21b4b009
Romina Rivera <rdrivera@uc.cl>
2025-03-16 00:11:18 -0300

    Agregar Express como dependencia y configurar el servidor en index.js

--- clase-expres-rasa/index.js
+++ clase-expres-rasa/index.js
@@ -1,1 +1,7 @@
-console.log('hello world');
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.listen(port, () => {
+  console.log('servidor escuchando el el puerto 4000');
+});
