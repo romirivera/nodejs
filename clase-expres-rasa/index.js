f73222d8d71042351c64265a867ff91ba6c5eee4
Romina Rivera <rdrivera@uc.cl>
2025-03-16 01:13:41 -0300

    Agregar rutas para manejar papayas: obtener, crear y eliminar

--- clase-expres-rasa/index.js
+++ clase-expres-rasa/index.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+const papayasrouter = express.Router();
 
+const ninfas = ['papaya', 'manguito'];
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ mensaje: 'tutrituri' }); //enviar en json a traves del endpoint creado en una api
+});
+
+papayasrouter.get('/papayas', (req, res) => {
+  res.json({ papayas: ninfas });
+});
+
+papayasrouter.post('/papayas', (req, res) => {
+  const newNinfa = req.body.nombre;
+  console.log(newNinfa);
+  ninfas.push(newNinfa);
+  res.json({ mensaje: 'papaya creada' });
+});
+
+papayasrouter.delete('/papayas', (req, res) => {
+  const deleteNinfa = req.body.nombre;
+  const indiceNinfaBorrada = ninfas.indexOf(deleteNinfa);
+  ninfas.splice(indiceNinfaBorrada, 1);
+  res.json({ mensaje: 'papaya eliminada' });
+});
+app.use(papayasrouter);
 app.listen(port, () => {
-  console.log('servidor escuchando el el puerto 4000');
+  console.log('servidor escuchando en el puerto 4000');
 });
