3c977aa803b600db3a89814a9d9961fa960cf7a1
Romina Rivera <rdrivera@uc.cl>
2025-03-18 17:23:17 -0300

    función para encontrar puertos disponibles

---
+++ clase-1/free-port.js
@@ -0,0 +1,24 @@
+//net es más rapido
+
+const net = require('node:net');
+
+function findAvailablePort(desiredPort) {
+  return new Promise((resolve, reject) => {
+    const server = net.createServer();
+
+    server.listen(desiredPort, () => {
+      const { port } = server.adress();
+      server.close(() => {
+        resolve(port);
+      });
+    });
+    server.on('error', (err) => {
+      if (err.code === 'EADRINUSE') {
+        findAvailablePort(0).then((port) => resolve(port));
+      } else {
+        reject(err);
+      }
+    });
+  });
+}
+module.exports = { findAvailablePort };
