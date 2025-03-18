c0c387d811f9575eb3eba044ccef4fca79f4c1ba
Romina Rivera <rdrivera@uc.cl>
2025-03-17 23:59:43 -0300

    Agregar ejemplo de lectura de archivos sincrónica y asincrónica

---
+++ clase-1/fs-readFile-sync.js
@@ -0,0 +1,15 @@
+const fs = require('node:fs');
+
+console.log('Leyendo el primer archivo...');
+const text = fs.readFileSync('./archivo.txt', 'utf-8');
+console.log(text);
+
+console.log('hacer cosas mientras se lee el archivo');
+
+console.log('Leyendo el segundo archivo...');
+
+const text2 = fs.readFile('./archivo2.txt', 'utf-8', (err, text) => {
+  console.log(text);
+});
+console.log('hacer cosas mienras se lee el archivo');
+console.log(text2);
